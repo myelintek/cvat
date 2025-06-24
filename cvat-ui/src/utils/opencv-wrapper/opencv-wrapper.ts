79b40f0fd605c0451397d53287bfdf9b285797b6
SimonChuang <simon.s.chuang@gmail.com>
2025-06-24 09:41:28 +0800

    fix opencv

--- cvat-ui/src/utils/opencv-wrapper/opencv-wrapper.ts
+++ cvat-ui/src/utils/opencv-wrapper/opencv-wrapper.ts
@@ -61,7 +61,7 @@ export class OpenCVWrapper {
     }
 
     private async inject(): Promise<void> {
-        const response = await fetch('/assets/opencv.js');
+        const response = await fetch('__PREFIX_URL__/assets/opencv.js');
         if (response.status !== 200) {
             throw new Error(`Response status ${response.status}. ${response.statusText}`);
         }
